66f2bedc9a511295904dd945fba1995dcdcf77c0
Rodrigo Gomes <cklrod@hotmail.com>
2022-06-20 22:30:36 -0300

    trabalhando com relacionamento

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -10,6 +10,47 @@ class PessoaController {
         }
 
     }
+
+    static async buscaUmaPessoa(req, res) {
+        try {
+            const { id } = req.params;
+            const pessoa = await database.Pessoas.findOne({ where: { id: Number(id) } });
+            return res.status(200).json(pessoa);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async inserirPessoa(req, res) {
+        const pessoa = req.body;
+        try {
+            const pessoaInserida = await database.Pessoas.create(pessoa);
+            return res.status(200).json({ message: 'Inserida com sucesso' });
+        } catch (error) {
+            res.status(500).json(error.message);
+        }
+    }
+
+    static async atualizarPessoa(req, res) {
+        const { id } = req.params;
+        const novosDados = req.body;
+        try {
+            await database.Pessoas.update(novosDados, { where: { id: Number(id) } });
+            return res.status(200).json({ message: 'Atualiza com sucesso' })
+        } catch (error) {
+            res.status(500).json(error.message);
+        }
+    }
+
+    static async deletarPessoa(req, res) {
+        const { id } = req.params;
+        try {
+            await database.Pessoas.destroy({ where: { id: Number(id) } });
+            return res.status(200).send('Deletado com sucesso!');
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
 }
 
 module.exports = PessoaController;
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -1,8 +1,13 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const PessoaController = require('../controllers/PessoaController')
 
 const router = Router();
 
-router.get('/pessoa', PessoaController.buscaPessoa);
+router
+    .get('/pessoas', PessoaController.buscaPessoa)
+    .get('/pessoas/:id', PessoaController.buscaUmaPessoa)
+    .post('/pessoas', PessoaController.inserirPessoa)
+    .put('/pessoas/:id', PessoaController.atualizarPessoa)
+    .delete('/pessoas/:id', PessoaController.deletarPessoa)
 
 module.exports = router;
