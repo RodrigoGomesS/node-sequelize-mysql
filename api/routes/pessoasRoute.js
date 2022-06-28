d05ac7c10227894425b9e62015c273bb33cd856c
Rodrigo Gomes <cklrod@hotmail.com>
2022-06-27 23:17:02 -0300

    soft delete com sequelize

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -11,6 +11,7 @@ router
     .delete('/pessoas/:id', PessoaController.deletarPessoa)
     .get('/pessoas/:estudanteId/matriculas/:matriculaId', PessoaController.buscaUmaMatricula)
     .post('/pessoas/:estudanteId/matriculas', PessoaController.inserirMatricula)
+    .post('/pessoas/:id/restaura', PessoaController.restauraPessoa)
     .put('/pessoas/:estudanteId/matriculas/:matriculaId', PessoaController.atualizarMatricula)
     .delete('/pessoas/:estudanteId/matriculas/:matriculaId', PessoaController.deletarMatricula)
 
